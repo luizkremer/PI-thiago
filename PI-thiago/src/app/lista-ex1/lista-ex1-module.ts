2d28f9db078ec8b7165cd6f75dd07c0a4054b3d2
luizkremer <luizkremere@gmail.com>
2026-09-05 18:41:11 +0000

    mm

--- PI-thiago/src/app/lista-ex1/lista-ex1-module.ts
+++ PI-thiago/src/app/lista-ex1/lista-ex1-module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { Ex1 } from './ex1/ex1';
+import { Ex2 } from './ex2/ex2';
+
 import { ListaEx1RoutingModule } from './lista-ex1-routing-module';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, ListaEx1RoutingModule],
+  declarations: [
+    Ex1,
+    Ex2
+  ],
+  imports: [
+    CommonModule,
+    ListaEx1RoutingModule
+  ]
 })
 export class ListaEx1Module {}
--- PI-thiago/src/app/lista-ex1/lista-ex1-routing-module.ts
+++ PI-thiago/src/app/lista-ex1/lista-ex1-routing-module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { Ex1 } from './ex1/ex1';
+import { Ex2 } from './ex2/ex2';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: Ex1
+  },
+  {
+    path: 'ex2',
+    component: Ex2
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class ListaEx1RoutingModule {}
