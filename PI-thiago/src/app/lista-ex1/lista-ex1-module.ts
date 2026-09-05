82246be232f870b76d4179b069ad0ed1da0e9e0c
luizkremer <luizkremere@gmail.com>
2026-09-05 18:08:27 +0000

    Cria modulo lista-ex1

---
+++ PI-thiago/src/app/lista-ex1/lista-ex1-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ListaEx1RoutingModule } from './lista-ex1-routing-module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ListaEx1RoutingModule],
+})
+export class ListaEx1Module {}
